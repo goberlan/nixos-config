8fa9b2904663f36aeadef452b3dfe86ae14e17c6
Glenn Oberlander <goberlan@pm.me>
2025-11-18 21:07:12 -0800

    improve to simplify build and boot

--- make-and-boot-vm.sh
+++ make-and-boot-vm.sh
@@ -1,4 +1,31 @@
-mkdir -p ~/vm/nixos
-fallocate -l 60G ~/vm/nixos/nixos.img
-qemu-system-x86_64 -enable-kvm -cdrom ~/Downloads/nixos-minimal-25.05.812778.3acb677ea67d-x86_64-linux.iso -m 32G -boot order=d -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
--drive if=pflash,format=raw,file=/home/whompyjaw/vm/nixos/ovmf_vars.4m.fd -drive file=~/vm/nixos/nixos.img,format=raw -nic user,hostfwd=tcp::6022-:22 -vga std
+# Change these as you need...
+VM_PORT=6022
+OVMF_ROOT=/usr/share/edk2/x64/
+VM_DIR=$HOME/vm/nixos
+NIX_ISO=$VM_DIR/nixos.iso
+
+mkdir -p $VM_DIR
+
+if [[ ! -f $NIX_ISO ]]; then
+    echo "No iso file. Getting it!"
+    wget --output-document $NIX_ISO https://channels.nixos.org/nixos-25.05/latest-nixos-minimal-x86_64-linux.iso 
+fi
+
+if [[ ! -f $HOME/vm/nixos/OVMF_VARS.4m.fd ]]; then
+    echo "No ovmf vars file. This is needed for UEFI booting. Copying..." 
+    cp $OVMF_ROOT/OVMF_VARS.4m.fd $HOME/vm/nixos
+fi
+
+
+# I tried the `qemu-img` command but it didn't really work...
+if [[ ! -f $VM_DIR/nixos.img ]]; then
+    echo "No image exists. Creating now."
+    fallocate -l 60G $VM_DIR/nixos.img
+fi
+
+# Boot VM
+# man qemu-system for info on these options used.
+# also: https://wiki.archlinux.org/title/QEMU
+echo "Booting VM in background."
+qemu-system-x86_64 -enable-kvm -cdrom $NIX_ISO -m 32G -boot order=d -drive if=pflash,format=raw,readonly=on,file=$OVMF_ROOT/OVMF_CODE.4m.fd \
+-drive if=pflash,format=raw,file=$HOME/vm/nixos/OVMF_VARS.4m.fd -drive file=$VM_DIR/nixos.img,format=raw -nic user,hostfwd=tcp::$VM_PORT-:22 -vga std &

ec4d0193fd434165b0a68c8285e880ee26d0c37c
whompyjaw <goberlan@pm.me>
2026-04-13 00:14:58 -0700

    fix hyprland

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -3,6 +3,14 @@
       enable = true;
       settings = {
         "$mod" = "SUPER";
+        input = {
+          kb_layout = "us";
+          kb_variant = "colemak_dh";
+          touchpad = {
+            disable_while_typing = false;
+            natural_scroll = true;
+          };
+        };
         bind = [
           "$mod, return, exec, ghostty +new-window" # ghostty should auto run as a d-bus/systemd app so this should work
           "$mod, Q, killactive"

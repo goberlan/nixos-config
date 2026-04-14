1c94ecaf2a9278408e26c6d553041892c61fa304
whompyjaw <goberlan@pm.me>
2026-04-13 21:21:53 -0700

    fix install setup

--- disko.sh
+++ disko.sh
@@ -1,3 +1,2 @@
 HOST=$1
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount
-/tmp/nixos-config/hosts/$HOST/disko.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /tmp/nixos-config/hosts/$HOST/disko.nix
--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 HOST=$1
-sudo nixos-build --flake /tmp/nixos-config#$HOST
+sudo nixos-install --flake /tmp/nixos-config#$HOST
+echo "Now that it's installed, we will reboot (that's all that's needed). Remember to change password for wj!"
+sleep 5
+reboot

c69ee75fc6d5a024cecfdb4fbcdcc97ae576894f
Glenn Oberlander <goberlan@pm.me>
2026-04-21 14:24:26 -0700

    tex: add

--- home/default.nix
+++ home/default.nix
@@ -1,12 +1,32 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  tex = (
+    pkgs.texliveBasic.withPackages (
+      ps: with ps; [
+        dvisvgm
+        dvipng # for preview and export as html
+        wrapfig
+        amsmath
+        ulem
+        hyperref
+        capt-of
+        #(setq org-latex-compiler "lualatex")
+        #(setq org-preview-latex-default-process 'dvisvgm)
+      ]
+    )
+  );
+in
+{
   home.username = "wj";
   home.homeDirectory = "/home/wj";
   home.stateVersion = "25.11";
   # may not need this... may be better to use programs.<prog>.enable
-  # home.packages = with pkgs; [
-  #   zig
-  # ];
-    
+  home.packages = with pkgs; [
+    zig
+    tex
+
+  ];
+
   imports = [
     ./hyprland.nix
     ./waybar.nix

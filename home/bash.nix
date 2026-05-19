d62be178dfb7cb78f42e09e22fc749ef459519a0
Glenn Oberlander <goberlan@pm.me>
2026-05-18 18:38:49 -0700

    rust: add shell function to init project

--- home/bash.nix
+++ home/bash.nix
@@ -1,5 +1,16 @@
-{ ... }: {
+{ pkgs, ... }:
+let
+  rustInitProject = pkgs.writeShellApplication {
+    name = "rust-init-project";
+    text = ''
+      nix flake init -t ~/nixos-config#rust
+    '';
+  };
+in
+{
+  home.packages = [ rustInitProject ];
   programs.bash = {
     enable = true;
   };
+
 }
